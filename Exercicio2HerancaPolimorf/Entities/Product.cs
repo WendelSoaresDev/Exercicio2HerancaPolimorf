d75d00d4d752d6373d362f50d3661add02103f24
Wendel Soares <wendel.soares.dev@gmail.com>
2022-02-22 05:40:34 -0300

    criada a entitade produto

---
+++ Exercicio2HerancaPolimorf/Entities/Product.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace Exercicio2HerancaPolimorf.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string PriceTag()
+        {
+            return Name + " $ " + Price;
+        }
+    }
+}
---
+++ Exercicio2HerancaPolimorf/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Exercicio2HerancaPolimorf
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+
+        }
+    }
+}
